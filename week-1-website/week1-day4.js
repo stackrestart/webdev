e54d7bd7839f0c255b0c6ebbe5249f8ac5542f2a
stackrestart <stackrestart@gmail.com>
2025-11-04 21:45:33 -0600

    started async js

---
+++ week-1-website/week1-day4.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    return a+b;
+}
+let ans = sum(2,3);
+// console.log(ans)
+
+function sum1(n){
+    let addition = 0;
+    for(let i = 1; i <= n; i++){
+        addition = addition + i;
+    }
+    return addition;
+}
+let ans1 = sum1(10);
+// console.log(ans1);
